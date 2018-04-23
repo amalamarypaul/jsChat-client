1f729d52585952a2526f560455d3fd51ee72b3db
Sidharth D <sidharth363@gmail.com>
2018-04-23 15:06:29 +0530

    Alignment, styling of messages, new message

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,16 +1,35 @@
 import React from 'react';
-
+import Grid from 'material-ui/Grid';
 
 import Messages from './Messages.js';
 import NewMessage from './NewMessage.js'
 import Header from './Header';
 
+import '../App.css';
+
 const HomePage = () => (
   <div>
     <Header />
-    <Messages />
-    <NewMessage/>
+    <div className = 'home-container'>
+      <Messages />
+      <NewMessage/>
+    </div>
+
+
+    {/* <Grid alignItems = 'center' container = { true } direction = 'column' md = { 12 }>
+      <Grid md = { 12 } item = { true }>
+        <Messages />
+        <NewMessage/>
+      </Grid>
+    </Grid> */}
   </div>
 );
+
+const styles = {
+  container: {
+    margin: 'auto',
+    width: '50%',
+  }
+}
 
 export default HomePage;
